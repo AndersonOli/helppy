1f9634f628777b791d8fb94dc89bd8be8ccf9875
Anderson Oliveira <contactandersonoliveira@gmail.com>
2020-04-06 01:37:26 -0300

    corrigir future build

--- lib/includes/tab_news.dart
+++ lib/includes/tab_news.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:helppyapp/pages.dart';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
 
 class NewsTab extends StatefulWidget {
     @override
@@ -8,11 +10,24 @@ class NewsTab extends StatefulWidget {
 
 class _NewsTabState extends State<NewsTab> {
     String textosNoticia =
-        'Última atualização: 30/30/3030 às 30:30\n\n' +
+        'Última atualização: 00/00/0000 às 00:00\n\n' +
         'Teresina - Piauí\n' +
-        '\t\t\t\tCasos confirmados: 100\n' +
+        '\t\t\t\tCasos confirmados: 000\n' +
         'Brazil\n' +
-        '\t\t\t\tCasos confirmados: 1032';
+        '\t\t\t\tCasos confirmados: 000';
+
+    var url = 'http://newsapi.org/v2/everything?q=coronavirus&?country=br&apiKey=3aaaaf0e6ab44bdea5e9806c43ee6447';
+
+    int newsLenght = 0;
+    dynamic newsContent;
+
+    newsData(index) async {
+        setState(() async {
+            var response = await http.get(url);
+            newsContent = convert.jsonDecode(response.body);
+            newsLenght = newsContent["articles"].length;
+        });
+    }
 
     @override
     Widget build(BuildContext context) {
@@ -47,8 +62,8 @@ class _NewsTabState extends State<NewsTab> {
                     ),
                     Expanded(
                         child: ListView.builder(
-                            itemCount: 20,
-                            itemBuilder: _buildNews
+                            itemCount: newsLenght,
+                            itemBuilder: _buildNews,
                         ),
                     ),
                 ],
@@ -57,7 +72,7 @@ class _NewsTabState extends State<NewsTab> {
     }
 
     Widget _buildNews(BuildContext context, int index){
-        if(index == 19){
+        if(index == newsLenght-1){
             return Column(
                 children: <Widget>[
                     GestureDetector(
@@ -74,7 +89,7 @@ class _NewsTabState extends State<NewsTab> {
                                                 bottomLeft: Radius.circular(4.0),
                                             ),
                                             child: Image.network(
-                                                "https://s2.glbimg.com/9Z8xUSj_yUjNB2aKad08hB8-iUg=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_e84042ef78cb4708aeebdf1c68c6cbd6/internal_photos/bs/2020/k/f/ZQZAh6Sueplvam3fMcDw/bbb20-050420-232523.jpg",
+                                                newsContent["articles"]["urlToImage"],
                                                 width: 150.0,
                                                 height: 100.0,
                                                 fit: BoxFit.fill,
@@ -83,7 +98,7 @@ class _NewsTabState extends State<NewsTab> {
                                         Expanded(
                                             child: Container(
                                                 padding: EdgeInsets.only(left: 10.0, right: 10.0),
-                                                child: Text("Gabi Martins foi eliminada do BBB (Big Brother Brasil) 20. Ela perdeu no paredão que disputava contra Thelma e Babu …",
+                                                child: Text(newsContent["articles"]["description"],
                                                     textAlign: TextAlign.justify,),
                                             ),
                                         )
--- lib/pages.dart
+++ lib/pages.dart
@@ -3,3 +3,5 @@ export 'includes/tab_home.dart';
 export 'includes/tab_help.dart';
 export 'includes/tab_news.dart';
 export 'package:helppyapp/includes/hellpy_icons_icons.dart';
+
+export 'helpers/news_api.dart';
