b65c281108407460e74be1494128eaefdf9f9140
Anderson Oliveira <contactandersonoliveira@gmail.com>
2020-04-12 20:21:06 -0300

    Tom de azul alterado

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-const Color COR_AZUL = Color.fromRGBO(0, 73, 255, 1);
+//const Color COR_AZUL = Color.fromRGBO(0, 73, 255, 1);
+const Color COR_AZUL = Color.fromRGBO(0, 59, 131, 1);
 const Color COR_CINZA = Color.fromRGBO(161, 161, 161, 1);
 const Color COR_BRANCO = Color.fromRGBO(240, 240, 240, 1);
 const Color COR_STROKE = Color.fromRGBO(230, 227, 227, 1);
