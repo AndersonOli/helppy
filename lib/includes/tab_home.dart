645fee22edb65994b865106c1195f8110cef39ca
Anderson Oliveira <contactandersonoliveira@gmail.com>
2020-04-22 05:05:49 -0300

    cards iniciados

--- lib/includes/tab_home.dart
+++ lib/includes/tab_home.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:helppyapp/globals.dart';
 
 class HomeTab extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        color: Colors.blue
-    );
-  }
+    @override
+    Widget build(BuildContext context) {
+        final _width = MediaQuery.of(context).size.width;
+        final _height = MediaQuery.of(context).size.height;
+        return SingleChildScrollView(
+            child: Container(
+                width: _width,
+                height: _height,
+                padding: EdgeInsets.only(right: 10.0, left: 10.0),
+                child: ListView.builder(
+                    itemCount: 10,
+                    shrinkWrap: true,
+                    itemBuilder: (context, index){
+                        return _cardPedido(context);
+                    },
+                ),
+            ),
+        );
+    }
+
+    Widget _cardPedido(context){
+        final _width = MediaQuery.of(context).size.width;
+        return Container(
+            width: _width,
+            height: 200.0,
+            margin: EdgeInsets.only(top: 10.0),
+            decoration: BoxDecoration(
+                color: COR_AZUL,
+                borderRadius: BorderRadius.circular(10.0)
+            ),
+        );
+    }
 }
