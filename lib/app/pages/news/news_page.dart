5e8f75768bc7c9219ba32603da1a7dcf4362a1f4
Anderson Oliveira <contactandersonoliveira@gmail.com>
2020-06-23 17:05:50 -0300

    api news updated

--- lib/app/pages/news/news_page.dart
+++ lib/app/pages/news/news_page.dart
@@ -6,59 +6,60 @@ import 'dart:convert' as convert;
 import 'package:http/http.dart' as http;
 
 class NewsTab extends StatefulWidget {
-    @override
-    _NewsTabState createState() => _NewsTabState();
+  @override
+  _NewsTabState createState() => _NewsTabState();
 }
 
-class _NewsTabState extends State<NewsTab> with AutomaticKeepAliveClientMixin<NewsTab> {
-    Future _futureData;
+class _NewsTabState extends State<NewsTab>
+    with AutomaticKeepAliveClientMixin<NewsTab> {
+  Future _futureData;
 
-    @override
-    void initState() {
-        super.initState();
-        _futureData = newsData();
-    }
+  @override
+  void initState() {
+    super.initState();
+    _futureData = newsData();
+  }
 
-    Future<List> newsData() async {
-        var response = await http.get('http://newsapi.org/v2/everything?language=pt&q=coronavirus brazil&?country=br&apiKey=3aaaaf0e6ab44bdea5e9806c43ee6447');
-        return [convert.jsonDecode(response.body)];
-    }
+  Future<List> newsData() async {
+    //http://newsapi.org/v2/everything?language=pt&q=coronavirus brazil&?country=br&apiKey=3aaaaf0e6ab44bdea5e9806c43ee6447
+    //
+    var response = await http.get(
+        'http://newsapi.org/v2/everything?language=pt&q=covid%20brasil&?country=br&apiKey=3aaaaf0e6ab44bdea5e9806c43ee6447');
+    return [convert.jsonDecode(response.body)];
+  }
 
-    @override
-    Widget build(BuildContext context) {
-        super.build(context);
-        return Padding(
-            padding: EdgeInsets.only(top: 10.0, left: 10.0, right: 10.0),
-            child: Column(
-                children: <Widget>[
-                    Expanded(
-                        child: FutureBuilder(
-                            future: _futureData,
-                            builder: (context, snapshot) {
-                                if(snapshot.connectionState == ConnectionState.waiting){
-                                    return loadingCenter();
-                                } else {
-                                    if(snapshot.hasData){
-                                        return BuildNews(snapshot);
-                                    } else {
-                                        return Center(
-                                            child: Text(
-                                                "Verifique sua conexão com a internet.",
-                                                style: TextStyle(
-                                                    color: COR_AZUL,
-                                                    fontSize: 14.0
-                                                ),
-                                            ),
-                                        );
-                                    }
-                                }
-                            }),
-                    ),
-                ],
-            ),
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return Padding(
+      padding: EdgeInsets.only(top: 10.0, left: 10.0, right: 10.0),
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: FutureBuilder(
+                future: _futureData,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return loadingCenter();
+                  } else {
+                    if (snapshot.hasData) {
+                      return BuildNews(snapshot);
+                    } else {
+                      return Center(
+                        child: Text(
+                          "Verifique sua conexão com a internet.",
+                          style: TextStyle(color: COR_AZUL, fontSize: 14.0),
+                        ),
+                      );
+                    }
+                  }
+                }),
+          ),
+        ],
+      ),
+    );
+  }
 
-    @override
-    bool get wantKeepAlive => true;
+  @override
+  bool get wantKeepAlive => true;
 }
